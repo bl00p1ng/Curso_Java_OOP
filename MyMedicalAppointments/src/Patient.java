aa952476c0df5d01f4b02abd533ecda8c3bb132d
bl00p1ng <blooping@protonmail.com>
2021-01-05 16:05:07 -0500

    Clase 11. Reto: Craer la clase Patient

---
+++ MyMedicalAppointments/src/Patient.java
@@ -0,0 +1,19 @@
+public class Patient {
+
+    String name;
+    String email;
+    String address;
+    String phoneNumber;
+    String birthday;
+    double weight;
+    double height;
+    String blood;
+
+    Patient(String name, String email) {
+
+        this.name = name;
+        this.email = email;
+
+    }
+
+}
